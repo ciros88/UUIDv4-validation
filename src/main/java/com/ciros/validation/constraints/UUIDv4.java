6a6a9591d8e0061d8a8735d901f5eea73bf819c6
Ciro Scognamiglio <ciro.scognamiglio88@gmail.com>
2021-12-16 11:09:42 +0100

    init

---
+++ src/main/java/com/ciros/validation/constraints/UUIDv4.java
@@ -0,0 +1,40 @@
+package com.ciros.validation.constraints;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Pattern.Flag;
+
+/**
+ * <p>
+ * The annotated element must be a valid version 4 UUID (case insensitive).
+ * <p>
+ * Accepts {@code CharSequence}.
+ * <p>
+ * {@code null} elements are considered valid.
+ *
+ * @author Ciro Scognamiglio
+ * 
+ */
+
+@Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}", flags = Flag.CASE_INSENSITIVE, message = "must be a valid version 4 UUID")
+
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.CONSTRUCTOR,
+		ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {})
+@Documented
+public @interface UUIDv4 {
+
+	String message() default "";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+}
