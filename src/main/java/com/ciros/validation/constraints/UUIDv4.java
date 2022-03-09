1b92b1e752bcea6e66d4eda030a5ef0c0b26ca19
Ciro Scognamiglio <c.scognamiglio@techgap.it>
2022-03-09 15:42:48 +0100

    null check added

--- src/main/java/com/ciros/validation/constraints/UUIDv4.java
+++ src/main/java/com/ciros/validation/constraints/UUIDv4.java
@@ -8,6 +8,7 @@ import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Pattern.Flag;
 
@@ -17,19 +18,19 @@ import javax.validation.constraints.Pattern.Flag;
  * <p>
  * Accepts {@code CharSequence}.
  * <p>
- * {@code null} elements are considered valid.
+ * {@code null} elements are considered invalid.
  *
  * @author Ciro Scognamiglio
  * 
  */
 
-@Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}", flags = Flag.CASE_INSENSITIVE, message = "must be a valid version 4 UUID")
-
 @Target({ ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.CONSTRUCTOR,
         ElementType.ANNOTATION_TYPE, ElementType.TYPE_USE })
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = {})
 @Documented
+@NotNull
+@Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}", flags = Flag.CASE_INSENSITIVE, message = "must be a valid version 4 UUID")
+@Constraint(validatedBy = {})
 public @interface UUIDv4 {
 
     String message() default "";
