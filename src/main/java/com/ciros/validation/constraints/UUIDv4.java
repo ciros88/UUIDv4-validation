9f98ccc99b244e088feea03337bc1ef43363bd9f
Ciro Scognamiglio <c.scognamiglio@techgap.it>
2022-03-15 19:17:56 +0100

    cleanup, gitignore updated

--- src/main/java/com/ciros/validation/constraints/UUIDv4.java
+++ src/main/java/com/ciros/validation/constraints/UUIDv4.java
@@ -1,7 +1,13 @@
 package com.ciros.validation.constraints;
 
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -23,8 +29,7 @@ import javax.validation.constraints.Pattern.Flag;
  * 
  */
 
-@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.CONSTRUCTOR,
-        ElementType.ANNOTATION_TYPE, ElementType.TYPE_USE })
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}", flags = Flag.CASE_INSENSITIVE, message = "must be a valid version 4 UUID")
